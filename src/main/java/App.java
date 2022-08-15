31d2a2bf09ac452e05c84ca0e91293e31a873535
xxxradoxxx <trustus@inbox.ru>
2022-08-15 21:29:15 +0300

    done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,13 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld world1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld world2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(world1 == world2);
+        System.out.println(cat1 == cat2);
     }
 }
